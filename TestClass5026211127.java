c8a696be9f74dafa4a7e8558baf84323bbcfd0ba
aisadira <100084532+aisadira@users.noreply.github.com>
2022-02-27 20:41:49 +0700

    Add files via upload

---
+++ TestClass5026211127.java
@@ -0,0 +1,12 @@
+
+public class TestClass5026211127	{
+	public static void main (String[] args)	{
+
+		ComputeMethods5026211127 test1 = new ComputeMethods5026211127();
+
+		System.out.println(test1.fToC(90));
+		System.out.println(test1.hypotenuse (4,3));
+		System.out.println(test1.roll ());
+
+	}
+}
